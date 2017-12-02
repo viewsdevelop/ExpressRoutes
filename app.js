96c4b7fe8cde61a8b3978f6d031f0959ca6c16d4
Louis Medina <viewsdevelop@gmail.com>
2017-12-01 23:04:27 -0500

    Remove code from app.js

--- app.js
+++ app.js
@@ -12,40 +12,32 @@ app.listen(3000, () => {
   console.log('My first app listening on port 3000!')
 });
 
+<<<<<<< Updated upstream
+=======
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'ejs');
+
+>>>>>>> Stashed changes
 // ----- Routes -----
 
 // Our first route
-app.get('/', (request, response, next) => {
-  console.log(request);
-  response.send('<p>Welcome Ironhacker. :)</p>');
+app.get('/', (req, res, next) => {
+  // console.log(request);
+  res.render('index');
 })
 
 // Our second route incorporating CSS
-app.get('/hello', (request, response, next) => {
-  response.send(`
-    <!doctype html>
-    <html>
-      <head>
-        <link rel="stylesheet" href="stylesheets/style.css">
-      </head>
-      <body>
-        This is my second route.
-      </body>
-    </html>
-  `);
-});
-
-// Save for 'index.ejs' file
-
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="utf-8">
-//   <title>My First View</title>
-//   <link rel="stylesheet" href="stylesheet/style.css">
-// </head>
-// <body>
-//   <h1>Ironhacker Be Like</h1>
-//   <img src="https://media.giphy.com/media/l0MYEqEzwMWFCg8rm/giphy.gif">
-// </body>
-// </html>
+// app.get('/hello', (request, response, next) => {
+//   response.send(`
+//     <!doctype html>
+//     <html>
+//       <head>
+//         <link rel="stylesheet" href="stylesheets/style.css">
+//       </head>
+//       <body>
+//         This is my second route.
+//       </body>
+//     </html>
+//   `);
+// });
