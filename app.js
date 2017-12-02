bb96507886b531a353659b177b2fc3af52330dd3
Louis Medina <viewsdevelop@gmail.com>
2017-12-01 21:29:36 -0500

    Add reference code to app.js

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.listen(3000, () => {
   console.log('My first app listening on port 3000!')
 });
 
+app.set('views', __dirname + '/views');
+
 // ----- Routes -----
 
 // Our first route
@@ -34,3 +36,18 @@ app.get('/hello', (request, response, next) => {
     </html>
   `);
 });
+
+// Save for 'index.ejs' file
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="utf-8">
+//   <title>My First View</title>
+//   <link rel="stylesheet" href="stylesheet/style.css">
+// </head>
+// <body>
+//   <h1>Ironhacker Be Like</h1>
+//   <img src="https://media.giphy.com/media/l0MYEqEzwMWFCg8rm/giphy.gif">
+// </body>
+// </html>
